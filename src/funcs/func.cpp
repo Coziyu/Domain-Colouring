4223408890695eff815d50ab7b2fabe2a5632f25
Nicholas <nicholas0295@gmail.com>
2022-06-19 15:38:55 +0800

    cleaned up old code

--- src/funcs/func.cpp
+++ src/funcs/func.cpp
@@ -10,37 +10,6 @@ void processInput(GLFWwindow *window){
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
         glfwSetWindowShouldClose(window, true);
     }
-    if (glfwGetKey(window, GLFW_KEY_CAPS_LOCK) == GLFW_PRESS){
-        const float def_vertices[12] = {
-            0.5f,  0.5f,  0.0f,
-            0.5f, -0.5f,  0.0f,
-            -0.5f, -0.5f,  0.0f,
-            -0.5f,  0.5f,  0.0f
-        };
-        for(int i = 0; i < 9; ++i){
-            vertices[i] = def_vertices[i];
-        }
-    }
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-        vertices[1] += 0.01;
-        vertices[4] += 0.01;
-        vertices[7] += 0.01;
-    }
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-        vertices[1] -= 0.01;
-        vertices[4] -= 0.01;
-        vertices[7] -= 0.01;
-    }
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        vertices[0] += 0.01;
-        vertices[3] += 0.01;
-        vertices[6] += 0.01;
-    }
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        vertices[0] -= 0.01;
-        vertices[3] -= 0.01;
-        vertices[6] -= 0.01;
-    }
 }
 void reloadShader(GLFWwindow* window, Shader* shader){
     shader->reload("./shaders/vertexShader1.vert","./shaders/fragmentShader1.frag");
