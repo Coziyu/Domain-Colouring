563a568f8958e9e7c21ddd05a9920d9d80301a04
Nicholas <nicholas0295@gmail.com>
2022-06-23 16:40:50 +0800

    Removed unused left over files from original fork.
    Forked from my own repo: LearnOpenGL
    Removed:
    - Texture Class
    - texture files
    - STB

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@
 #include "headers/func.h"
 #include "headers/data.h"
 #include "headers/shader.h"
-#include "headers/texture.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb/stb_image.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
